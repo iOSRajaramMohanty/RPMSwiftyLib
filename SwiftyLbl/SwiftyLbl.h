d5a3ac1669aadf737751d12c660f63b0884ea756
Rajaram Mohanty <rajaram.purusottammohanty@publicissapient.com>
2020-03-17 11:48:02 +0530

    Initial Commit

---
+++ SwiftyLbl/SwiftyLbl.h
@@ -0,0 +1,19 @@
+//
+//  SwiftyLbl.h
+//  SwiftyLbl
+//
+//  Created by Rajaram Purusottam Mohanty on 17/03/20.
+//  Copyright © 2020 Rajaram Purusottam Mohanty. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SwiftyLbl.
+FOUNDATION_EXPORT double SwiftyLblVersionNumber;
+
+//! Project version string for SwiftyLbl.
+FOUNDATION_EXPORT const unsigned char SwiftyLblVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftyLbl/PublicHeader.h>
+
+
