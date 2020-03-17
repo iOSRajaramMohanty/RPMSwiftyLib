d2caca24b0377e580c431a313996dd1e6e4824c4
Rajaram Mohanty <rajaram.purusottammohanty@publicissapient.com>
2020-03-17 12:50:39 +0530

    Added SwiftyLib.podspec

---
+++ SwiftyLbl/SwiftyLib.swift
@@ -0,0 +1,23 @@
+//
+//  SwiftyLib.swift
+//  SwiftyLbl
+//
+//  Created by Rajaram Purusottam Mohanty on 17/03/20.
+//  Copyright © 2020 Rajaram Purusottam Mohanty. All rights reserved.
+//
+
+import Foundation
+
+public final class SwiftyLib {
+
+    let name = "SwiftyLib"
+    
+    public func add(a: Int, b: Int) -> Int {
+        return a + b
+    }
+    
+    public func sub(a: Int, b: Int) -> Int {
+        return a - b
+    }
+    
+}
--- SwiftyLblTests/SwiftyLblTests.swift
+++ SwiftyLblTests/SwiftyLblTests.swift
@@ -11,14 +11,24 @@ import XCTest
 
 class SwiftyLblTests: XCTestCase {
 
+    var swiftyLib: SwiftyLib!
+
     override func setUp() {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+        swiftyLib = SwiftyLib()
     }
 
     override func tearDown() {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
+    
+    func testAdd() {
+        XCTAssertEqual(swiftyLib.add(a: 1, b: 1), 2)
+    }
 
+    func testSub() {
+        XCTAssertEqual(swiftyLib.sub(a: 2, b: 1), 1)
+    }
+    
     func testExample() {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
